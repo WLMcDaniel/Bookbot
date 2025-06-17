5a1fe325c401ddf7f451eac7684ff6936c9a85bf
Argo <bmcdaniel9724@gmail.com>
2025-06-17 16:59:49 -0400

    Python Bookbot project

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-
+import sys
 def get_book_text(filepath):
     with open(filepath) as f:
     #Do something with f (The file) here   
     # f is a file object
         file_contents = f.read() 
-    return file_contents  
+    return file_contents 
+
+def check_fun():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        pass
+
+check_fun()
 
 def main():
-    book_text = get_book_text("../bookbot/books/frankenstein.txt")
-    print(book_text)
+    book_text = get_book_text(sys.argv[1])
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}")
+    print("----------- Word Count ----------")
+    words_in_doc(book_text)
+    print("--------- Character Count -------")
+    char_counts = character_appear(book_text)
+    sorted_chars = sort_dict(char_counts)
+    for item in sorted_chars:
+        char = item["char"]
+        num = item["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")  
+    print("============= END ===============")
+    
+    
+
+from stats import words_in_doc
+from stats import character_appear
+from stats import sort_dict
+
+
+
 
 main()
-    
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def words_in_doc(doc):
     new_list = doc.split()
     words = len(new_list)
-    print(f"{words} words found in the document")
+    print(f"Found {words} total words")
     return words
 
 def character_appear(doc):
@@ -14,3 +14,16 @@ def character_appear(doc):
             counts[i] = 1
     return counts
 
+def sort_on(dict_item):
+    return dict_item["num"]
+
+
+def sort_dict(char_dict):
+    new_list = []
+    for key, value in char_dict.items():
+        new_dict = {"char": key, "num": value}
+        new_list.append(new_dict)
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
+  
+    
